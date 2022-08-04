829b9296aba1606a40c31ed2a4c00a24339df19c
luc siat <lucsiat@gmail.com>
2022-08-04 13:53:42 +0200

    recovered the information and generated the json

--- mercedes.rb
+++ mercedes.rb
@@ -4,7 +4,6 @@ require 'open-uri'
 
 # mapbox_key = ENV['MAPBOX_API_KEY']
 
-# improve the way of sorting infos from the location, bad practice to include lat= and types=, try finding a cleaner solution
 
 # create the data and url
 test_location = {
@@ -13,16 +12,23 @@ test_location = {
   endpoint_type: "museum",
   types: "poi"
 }
-endpoint_url = "mapbox.places/#{test_location[:endpoint_type]}.json?types=#{test_location[:types]}&proximity=#{test_location[:longitude]},#{test_location[:latitude]}&"
 
-# use mapbox
+# send a request to mapbox
+endpoint_url = "mapbox.places/#{test_location[:endpoint_type]}.json?types=#{test_location[:types]}&proximity=#{test_location[:longitude]},#{test_location[:latitude]}&"
 mapbox_url = "https://api.mapbox.com/geocoding/v5/#{endpoint_url}access_token=#{ENV['MAPBOX_API_KEY']}"
 mapbox_request = URI.open(mapbox_url).read
 mapbox_museum_response = JSON.parse(mapbox_request)
-puts mapbox_museum_response
+
 
 # fetch the name, lat, lon of the museums
-
-# send a request to mapbox
+endpoint_type_nearby = {}
+mapbox_museum_response["features"].each do |museum|
+  museum_postcode = museum["context"][0]["text"]
+  museum_name = museum["text"]
+  endpoint_type_nearby[museum_postcode] = museum_name
+end
 
 # get the response and translate it to json
+File.open("endpoint_type_nearby.json", "wb") do |file|
+  file.write(JSON.generate(endpoint_type_nearby))
+end
