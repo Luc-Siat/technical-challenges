1992359b65cd304d80ba7b200a7e770ccd94cb40
luc siat <lucsiat@gmail.com>
2022-08-04 12:30:14 +0200

    created the mapbox url with endpoint and parse the json

--- mercedes.rb
+++ mercedes.rb
@@ -1,8 +1,25 @@
-mapbox_key = ENV['MAPBOX_API_KEY']
-
 require 'dotenv/load'
+require 'json'
+require 'open-uri'
 
-# implemant mapbox
+# mapbox_key = ENV['MAPBOX_API_KEY']
+
+# improve the way of sorting infos from the location, bad practice to include lat= and types=, try finding a cleaner solution
+
+# create the data and url
+test_location = {
+  longitude: "13.437641",
+  latitude: "52.494857",
+  endpoint_type: "museum",
+  types: "poi"
+}
+endpoint_url = "mapbox.places/#{test_location[:endpoint_type]}.json?types=#{test_location[:types]}&proximity=#{test_location[:longitude]},#{test_location[:latitude]}&"
+
+# use mapbox
+mapbox_url = "https://api.mapbox.com/geocoding/v5/#{endpoint_url}access_token=#{ENV['MAPBOX_API_KEY']}"
+mapbox_request = URI.open(mapbox_url).read
+mapbox_museum_response = JSON.parse(mapbox_request)
+puts mapbox_museum_response
 
 # fetch the name, lat, lon of the museums
 
