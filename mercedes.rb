631004d9b37a2a9c2efc76f79bf6b59ee711a13f
luc siat <lucsiat@gmail.com>
2022-08-04 11:16:20 +0200

    added env for the mapbox key

--- mercedes.rb
+++ mercedes.rb
@@ -1,1 +1,11 @@
-puts 'Github test'
+mapbox_key = ENV['MAPBOX_API_KEY']
+
+require 'dotenv/load'
+
+# implemant mapbox
+
+# fetch the name, lat, lon of the museums
+
+# send a request to mapbox
+
+# get the response and translate it to json
